35a0fcabfad2349a1dfff7497d8b481bdffe8f9c
Haoming Wei <haoming.wei321@gmail.com>
2024-04-23 16:01:40 +0800

    style(lab0): refractor code and comments in user/find.c

--- user/find.c
+++ user/find.c
@@ -5,26 +5,6 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/fs.h"
-
-char*
-fmtname(char *path)
-{
-    static char buf[DIRSIZ+1];
-    char *p;
-
-    // Find first character after last slash.
-    for(p=path+strlen(path); p >= path && *p != '/'; p--)
-        ;
-    p++;
-
-    // Return blank-padded name.
-    if(strlen(p) >= DIRSIZ)
-        return p;
-    memmove(buf, p, strlen(p));
-    memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-    return buf;
-}
-
 void
 find(char *path, char *target)
 {
@@ -45,7 +25,7 @@ find(char *path, char *target)
     }
     switch (st.type) {
         case T_FILE:
-            // Find first character after last slash.
+            // Find file name from path
             for(p=path+strlen(path); p >= path && *p != '/'; p--)
                 ;
             p++;
