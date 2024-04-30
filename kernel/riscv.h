148c95423a647e243416aea253baae5053f659d5
Haoming Wei <haoming.wei321@gmail.com>
2024-04-30 10:47:40 +0800

    feat(lab3): implement RISC-V assembly and Backtrace

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -7,6 +7,15 @@ r_mhartid()
   return x;
 }
 
+// store current frame pointer to s0
+static inline uint64
+r_fp()
+{
+    uint64 x;
+    asm volatile("mv %0, s0" : "=r" (x) );
+    return x;
+}
+
 // Machine Status Register, mstatus
 
 #define MSTATUS_MPP_MASK (3L << 11) // previous mode.
